77e87cacbe846ad5310c20d1f21e2569f3481288
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-16 19:53:54 +0600

    short hand way to declare array

--- lecture-10/main.go
+++ lecture-10/main.go
@@ -4,10 +4,7 @@ import (
 )
 
 func main() {
-	var student [3]string 
-	student[0]="jahid"
-	student[1]="jahid1"
-	student[2]="jahid2"
-	fmt.Println(student[0])
+	student:=[3]string{}
+	fmt.Println(student)
 	//fmt.Println(len(student))
 }

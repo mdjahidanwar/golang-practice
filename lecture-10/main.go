625fdb5bbf695e95df6e9e492a638cd793a1044f
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-18 01:45:16 +0600

    empty map

--- lecture-10/main.go
+++ lecture-10/main.go
@@ -1,19 +1,14 @@
 package main
 import (
 "fmt"
-"reflect"
 )
 
 func main() {
-a:=make([]string,0)
-fmt.Println(a)
 
-var fruits []string
-fruits = append(fruits, "apple","banana")
-fmt.Println(fruits)
-fmt.Printf("%T\n",fruits)
+ var x map[string]string
+// x["key"]=10
 
-b:=reflect.TypeOf(fruits).Kind().String()
-fmt.Println(b)
+fmt.Println(x)
+
 
 }

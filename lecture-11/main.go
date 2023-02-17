605e6c25bb35ae9896587c1ba21cfa995040a994
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-18 03:06:27 +0600

    added all the values in b1 book

--- lecture-11/main.go
+++ lecture-11/main.go
@@ -15,5 +15,9 @@ pages int
 func main() {
 var b1 book 
 b1.title="an introduction to programming in go"
+b1.author="caleb doxy"
+b1.ISBN="978-1478355823"
+b1.price=10.50
+b1.pages=165
 fmt.Println(b1)
 }

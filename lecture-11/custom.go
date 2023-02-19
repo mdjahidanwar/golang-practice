3e43f041e2e8e37415ff0630ac179a21ac1b19cd
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-19 22:48:35 +0600

     non-declaration statement outside function body

--- lecture-11/custom.go
+++ lecture-11/custom.go
@@ -1,2 +1,23 @@
 package main 
 type weight float32
+
+B1:= struct{
+title string 
+author string 
+pages int
+ISBN string 
+price float32
+}{
+title: "an introduction",
+author: "jahid",
+ISBN: "9x9x9x9xx9",
+price: 10.80,
+pages: 10,
+}
+//type book struct {
+//title string 
+//author string 
+//pages int 
+//ISBN string 
+//price float32
+//}
--- lecture-11/main.go
+++ lecture-11/main.go
@@ -2,34 +2,11 @@ package main
 import (
 "fmt"
 )
-//type book struct {
-//title string 
-//author string 
-//pages int 
-//ISBN string 
-//price float32
-//}
 
 func main() {
 //var b1 book
 
-b1:= struct{
-title string 
-author string 
-pages int
-ISBN string 
-price float32
-}{
-title: "an introduction",
-author: "jahid",
-ISBN: "9x9x9x9xx9",
-price: 10.80,
-pages: 10,
-}
-
-
-fmt.Println(b1)
-
+fmt.Println(B1)
 
 var w1 weight 
 w1=30.5

e9ca2be855b549721c91cde20680ff8a35d31113
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-19 22:56:00 +0600

    created statuct and assign value from a different file

--- lecture-11/custom.go
+++ lecture-11/custom.go
@@ -1,19 +1,14 @@
 package main 
 type weight float32
 
-B1:= struct{
+type b1 struct{
 title string 
 author string 
 pages int
 ISBN string 
 price float32
-}{
-title: "an introduction",
-author: "jahid",
-ISBN: "9x9x9x9xx9",
-price: 10.80,
-pages: 10,
 }
+var book b1
 //type book struct {
 //title string 
 //author string 
--- lecture-11/main.go
+++ lecture-11/main.go
@@ -6,7 +6,14 @@ import (
 func main() {
 //var b1 book
 
-fmt.Println(B1)
+
+book.title= "an introduction"
+book.author= "jahid"
+book.ISBN= "9x9x9x9xx9"
+book.price= 10.80
+book.pages= 10
+
+fmt.Println(book)
 
 var w1 weight 
 w1=30.5

e0b410f1c56cd885b123449cd63dfc28174fef61
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-22 11:31:17 +0600

    else if, else block added

--- control flow - lecture 12/main.go
+++ control flow - lecture 12/main.go
@@ -12,7 +12,19 @@ func main() {
 
  if age < 3 {
 	fmt.Println("infant")
+}else if age >2 && age <13 {
+	fmt.Println ("children") 
+
+
+} else if age > 12 && age <= 19 {
+	fmt.Println("teenage")
+
+
+}else {
+	fmt.Println("adult")
+
 }
+
 
  //fmt.Println(age)
 

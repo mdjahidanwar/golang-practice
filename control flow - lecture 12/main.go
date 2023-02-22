a99b3bd86fe03f84c8aead3f4b9986f97d460fa1
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-22 11:36:48 +0600

    switch

--- control flow - lecture 12/main.go
+++ control flow - lecture 12/main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
+ 
+
  fmt.Print("Please print your age: ")
  var age int
  fmt.Scanf("%d",&age)
-
+/*
  if age < 3 {
 	fmt.Println("infant")
 }else if age >2 && age <13 {
@@ -24,6 +26,19 @@ func main() {
 	fmt.Println("adult")
 
 }
+*/
+
+switch age {
+	case 2:
+            fmt.Println("infant")
+	case 3,4,5,6,7,8,9,10,11,12:
+            fmt.Println("children")
+	case 13,14,15,16,17,18,19:
+            fmt.Println("teanage")
+	default:
+		fmt.Println("adult")
+}
+
 
 
  //fmt.Println(age)

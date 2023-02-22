84c455dbea706851a3254c4329eaa9efde7a3285
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-22 11:54:03 +0600

    print 1 to 9

---
+++ control-flow-lecture-12-for_loop/main.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+"fmt"
+
+)
+
+func main() {
+ 
+/*
+ fmt.Print("Please print your age: ")
+ var age int
+ fmt.Scanf("%d",&age)
+*/
+ for i:=1; i<=9; i++ {
+    fmt.Println (i)
+
+}
+
+}

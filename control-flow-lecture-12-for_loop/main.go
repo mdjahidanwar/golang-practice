b86632a9bda0de9b1985fe4cc476a9d61920598b
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-22 12:02:44 +0600

    unlimited hello

--- control-flow-lecture-12-for_loop/main.go
+++ control-flow-lecture-12-for_loop/main.go
@@ -12,16 +12,23 @@ func main() {
  var age int
  fmt.Scanf("%d",&age)
 */
+/*
  for i:=1; i<=9; i++ {
     fmt.Println (i)
 
 }
-
+*/
+/*
  students:=[]string{"jahid","mainul","jakir"}
  for i, std:=range students {
 	fmt.Println(i,std)
 } 
+*/
+for true {
+fmt.Println("hello")
 
+
+}
 
 
 }

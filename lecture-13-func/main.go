e2fd298ada6aefe8de0ca6aa13e2597e35d093dd
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-22 18:10:56 +0600

    multiple parameter

---
+++ lecture-13-func/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+//example 1
+/*
+func add(x int, y int) int {
+r:=x+y
+return r
+}
+*/
+
+//example 2
+/*
+func add(x, y int) int {
+r:=x+y
+return r
+}
+*/
+
+//example 3
+/*
+func add(x int, y int) (r int) {
+r=x+y
+return r
+}
+*/
+
+//example 4
+/*
+func add(x int, y int) (r int) {
+	r = x + y
+	return
+}
+*/
+
+func rectangle (l,b int) (area, parameter int ){
+parameter = 2*(l+b)
+area=l*b
+return 
+
+
+
+}
+
+
+
+
+func main() {
+
+//	x := add(10, 30)
+//	fmt.Println(x)
+a,p:= rectangle (10,10)
+fmt.Println(a,p)
+}

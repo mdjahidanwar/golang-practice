07f66fb7cd853ec867ca19ab3b64bdea25219772
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-14 03:34:36 +0600

    referencing memory location of x in y

--- lecture-14-pointer/main.go
+++ lecture-14-pointer/main.go
@@ -8,4 +8,8 @@ fmt.Println("value of x is: ",x)
 fmt.Println("memory address of x is: ",&x)
 fmt.Println ("value of y is: ",y)
 fmt.Println("memory address of y is: ",&y)
+x=10
+y=&x
+fmt.Println("value of x is: ",x)
+fmt.Println("value of y is: ",y)
 }

81672aeec66d77dd44f1a987d9329dd80d4e2be6
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-14 03:38:16 +0600

    naming

--- lecture-14-pointer/main.go
+++ lecture-14-pointer/main.go
@@ -8,9 +8,9 @@ fmt.Println("value of x is: ",x)
 fmt.Println("memory address of x is: ",&x)
 fmt.Println ("value of y is: ",y)
 fmt.Println("memory address of y is: ",&y)
-x=10
-y=&x
-fmt.Println("value of x is: ",x)
+x=10 //assignment 
+y=&x //referencing
+fmt.Println("value of x is: ",x) //accessing 
 fmt.Println("value of y is: ",y)
-fmt.Println("value of y is: ",*y)
+fmt.Println("value of y is: ",*y) //dereferencing
 }

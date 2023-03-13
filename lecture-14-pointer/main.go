7cf571f81d5b28144b81c77ec8c6a7a2c17928c4
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-14 03:23:56 +0600

    y pointer declared

--- lecture-14-pointer/main.go
+++ lecture-14-pointer/main.go
@@ -3,6 +3,9 @@ import "fmt"
 
 func main() {
 var x int 
+var y *int
 fmt.Println("value of x is: ",x)
 fmt.Println("memory address of x is: ",&x)
+fmt.Println ("value of y is: ",y)
+fmt.Println("memory address of y is: ",&y)
 }

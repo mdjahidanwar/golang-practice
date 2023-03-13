15854968267e01873b0d64d9194529653e9c99f9
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-14 04:49:35 +0600

    name change

--- practice/hello.go
+++ practice/hello.go
@@ -21,3 +21,17 @@ func numToChar(num int ) {
 func add(num1 int, num2 int) {
 	fmt.Println(num1+num2)
 }
+
+
+
+account balance 
+billing and payment 
+
+
+
+
+ref 
+request response 
+
+9Z@pfKV}3y!v
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAv+6F048K+/genyWOwjFk4T6u/q/IZgwQv0s5lvTrMGmAiGz+CnjshyLQVW47FPTaYdy+aNMtcH5V2dK792DCpZFjpw0m4qHOlvrQaU3IuwacRkTYGWLPGRDFGqwykwqGtqMO82lIsnPApLP6HEaF48AcQgTJzpM9X4WmxDCPbv9g0/JnW6YWr1b8UhjFCQ0OKMijrUS1OPdgtrK/IKe9Nzd24DxzmKQIxrXQhRiWSix4Wkgcn7hb2SE7PE2ljPIshssV2eRcBtlnlsKzty3JqvJYQj+p5UHc8GXZUrjNPDC5PEmsAcNVDOi42QG9aOctbl6zaS+5+vQRYoExyOWWlQ== mygpadmin@a0410tmgpdb01.grameenphone.com

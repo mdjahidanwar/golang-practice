8053e6ee3e5fae4065945ada2d6bbca687b4068f
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-17 01:49:45 +0600

    about and contact page addded

--- lecture-17-http-server/main.go
+++ lecture-17-http-server/main.go
@@ -11,10 +11,25 @@ import (
 
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+
 	http.ListenAndServe(":8080", nil)
 }
 func home(w http.ResponseWriter, r *http.Request) {
 
-    fmt.Fprintf(w,`welcome to my first golang web page`)
+	fmt.Fprintf(w, `welcome to my first golang web page`)
+
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `welcome to my first golang web page- about`)
+
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `welcome to my first golang web page- contact`)
 
 }

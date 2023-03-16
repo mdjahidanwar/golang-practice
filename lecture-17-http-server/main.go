ac759b1c96a9ca40837fca750d21c406e8bfd77e
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-17 01:45:36 +0600

    net-http package

---
+++ lecture-17-http-server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// type Handler interface {
+// 	ServeHTTP(ResponseWriter, *Request)
+// }
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":8080", nil)
+}
+func home(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Fprintf(w,`welcome to my first golang web page`)
+
+}

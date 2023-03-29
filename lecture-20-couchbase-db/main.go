986ef1e565dba7e6c92bfb808c355966400a6270
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-03-30 04:35:50 +0600

    couch db

---
+++ lecture-20-couchbase-db/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	//	"text/template"
+	"html/template"
+)
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/request", request)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8880", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+}
+
+func features(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+}
+
+func docs(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("./template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+	//	r.ParseForm()
+	//method ---1 
+	// name := r.FormValue("name")
+	// company := r.FormValue("company")
+	// email := r.FormValue("email")
+	// fmt.Println(name, company, email)
+	// fmt.Fprintf(w, `received %s %s %s`, name, company, email)
+
+	//methos 2 
+	r.ParseForm()
+	for key, val:=range r.Form{
+		fmt.Println(key, val)
+	}
+fmt.Fprintf(w, `received ok`,)
+
+}
+
+	//fmt.Fprintf(w, `welcome to golang`)
